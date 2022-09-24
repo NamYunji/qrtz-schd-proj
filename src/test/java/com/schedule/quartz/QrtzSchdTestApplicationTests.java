b9af1a592e4ca5f9389c9366c01471131f5aba67
NamYunji <dbs7wl7@gmail.com>
2022-09-24 21:00:53 +0900

    20220924 최초커밋

---
+++ src/test/java/com/schedule/quartz/QrtzSchdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedule.quartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrtzSchdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
